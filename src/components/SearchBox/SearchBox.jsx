b264491bca701254aa976fc3914d56d3685b704d
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-04-12 12:05:47 +0200

    1stepadd

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,13 @@
 import css from "./SearchBox.module.css";
 
 export default function SearchBox() {
+  const handleInput = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <>
       <p>Find contacts by name</p>
-      <input type="text"></input>
+      <input type="text" name="search" onChange={handleInput}></input>
     </>
   );
 }
