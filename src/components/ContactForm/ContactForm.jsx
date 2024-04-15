4edc34f69730cee08ece457bb20585a6f23221f4
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-04-15 17:17:04 +0200

    hopeforthelastamendments

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,6 @@
 import css from "./ContactForm.module.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { useId } from "react";
 import { nanoid } from "nanoid";
 
 const UserSchema = Yup.object().shape({
